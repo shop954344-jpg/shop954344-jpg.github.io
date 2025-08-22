f34ba5674be895fa8d8a0e5c2afed61a6ef4e2da
shop954344-jpg <shop954344@gmail.com>
2025-08-22 18:53:37 +0800

    Site updated: 2025-08-22 18:53:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["uncategorized/更新歷程/","遊戲設置/遊戲設置/","uncategorized/遊戲特色/","uncategorized/武器介紹/","uncategorized/防具介紹/","uncategorized/娃娃介紹/","uncategorized/道具介紹/","uncategorized/懶人包下載專區/"];function toRandomPost(){
+var posts=["uncategorized/懶人包下載專區/","uncategorized/更新歷程/","遊戲設置/遊戲設置/","uncategorized/遊戲特色/","uncategorized/武器介紹/","uncategorized/防具介紹/","uncategorized/娃娃介紹/","uncategorized/道具介紹/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
