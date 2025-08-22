ddef99a42ac0db8aa53d5d248466204822bcc178
shop954344-jpg <shop954344@gmail.com>
2025-08-22 16:56:04 +0800

    Site updated: 2025-08-22 16:56:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/12/懶人包下載專區/"];function toRandomPost(){
+var posts=["2025/08/12/懶人包下載專區/","2025/08/22/更新歷程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
